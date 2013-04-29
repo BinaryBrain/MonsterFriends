adf7380511561197113208f8f511bef1334eab27
Flagoul <basile.vu@gmail.com>
2013-04-29 16:50:24 +0200

    Static fight

--- app/static/js/fight.js
+++ app/static/js/fight.js
@@ -1,54 +1,61 @@
 $(function () {
+  // TODO: Load from server
   var trainers = [
-    { fb_id: 1063020932 },
-    { fb_id: 1236701567 }
+    { fb_id: 1063020932, name: "Sacha Bron" },
+    { fb_id: 1236701567, name: "Kewin Dousse" }
   ] 
   var monsters = [
-    { fb_id: 1063020932, pv_max: 200, pv: 130, level: 4 },
-    { fb_id: 1236701567, pv_max: 60, pv: 25, level: 1 }
+    { fb_id: 1063020932, pv_max: 200, pv: 130, level: 4, name: "Sacha Bron"},
+    { fb_id: 1236701567, pv_max: 60, pv: 25, level: 1, name: "Kewin Dousse"}
   ]
 
   $('#vs').html(getVsHtml(trainers))
   $('#battle').html(getBattleHtml(monsters))
-	
-})
-	
+  
+  Dialog.show()
+})	
 	
 function getVsHtml(trainers) {
   var html = ""
-  var name1 = "test1" // var name = Facebook.getFriendFromID(trainer[0].fb_id).name
-  var name2 = "test2"
-  html += "<div>"
-    html += name1
-    //html += '<img src="http://graph.facebook.com/'+trainers[0].fb_id+'/picture?width=40&height=40">'
+    html += getTrainersHtml(trainers,0)
     html += "<h2>VS</h2>"
-    //html += '<img src="http://graph.facebook.com/'+trainers[1].fb_id+'/picture?width=40&height=40">'
-    html += name2
-  html += "</div>"
+    html += getTrainersHtml(trainers,1)
 
   return html;
-}	
+}
+
+function getTrainersHtml(trainers,i){
+  var html = ""
+  html += "<div id='trainer"+i+"' class='trainer'>"
+    html += '<img src="http://graph.facebook.com/'+trainers[i].fb_id+'/picture?width=40&height=40">'
+    html += "<p>"+trainers[i].name+"</p>"
+  html += "</div>"
+  
+  return html
+}
 
 function getBattleHtml(monsters) {
   var html = ""
   for(var i = 0; i<monsters.length; i++) {
     html+="<div id='monsterContainer"+i+"' class='monsterContainer'>"
       html+="<div class='monster'>"
-	html+= '<img src="http://graph.facebook.com/'+monsters[i].fb_id+'/picture?width=100&height=100">'
+	html+= '<img src="http://graph.facebook.com/'+monsters[i].fb_id+'/picture?width=129&height=129">'
       html+="</div>"
       html+= getStatusHtml(monsters[i])
     html+="</div>"
   }
-  return html;
+  return html
 }
 	
 function getStatusHtml(monster) {
   var html = ""
-  var name = Facebook.getFriendFromID(monster.fb_id).name
-  html += '<div class="data">'
-    html += '<h4>'+name+' - lvl '+monster.level+'</h4>'
-    html += getLifeBarHtml(monster)
-  html += '</div>'
+  var name = monster.name //Facebook.getFriendFromID(monster.fb_id).name 
+  html+= "<div class='dataContainer'>"
+    html += "<div class='data'>"
+      html += "<h4>"+name+" - lvl "+monster.level+"</h4>"
+      html += getLifeBarHtml(monster)
+    html += "</div>"
+  html += "</div>"
 
   return html
 }
