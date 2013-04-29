984b1b54562a8605a8a704f4e99ccd516c441c02
Protectator <kewin.d@websud.ch>
2013-04-29 17:53:56 +0200

    Started History page

--- app/static/js/history.js
+++ app/static/js/history.js
@@ -1,3 +1,15 @@
 $("#previousButton").on('click', function () {
 	View.show(Scene.MENU)
 })
+
+$(function () {
+	Network.getMatchHistory(function(matchHistory) {
+		matchHistory = jQuery.parseJSON(matchHistory)
+		
+		var html = ""
+		
+		console.log(matchHistory)
+		
+		$("#history #fights").html(html)
+	})
+})
