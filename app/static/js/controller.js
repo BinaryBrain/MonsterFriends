20b6e32602d4b7914f469b0c88eee5964ceddd6e
Sacha Bron <sacha.bron@copperflake.com>
2013-05-01 15:52:07 +0200

    Dialog methods (taken from deprecated Console)

--- app/static/js/controller.js
+++ app/static/js/controller.js
@@ -22,7 +22,9 @@ $(function () {
 Controller = {
 	
   error: function (err) {
-    console.log("Error: "+err);
+    console.log("Error: "+err)
+    Dialog.show()
+    Dialog.print("Error: "+err)
   },
 
   init: function () {
@@ -55,78 +57,4 @@ Controller = {
       });
   },
 	
-}
-
-
-/*
- * Console Object
- *
- * Represents the Console during a fight. Used to show messages
- * 
- */
-Console = {
-  /*
-  archieveHistory: function (data) {
-	  for (var i = 0; i < data.length; i++) {
-		  
-	  }
-  },
-  */
-
-  hide: function () {
-    this.consoleNode.hide();
-  },
-
-  show: function () {
-    this.consoleNode.show();
-  },
-
-  archieveMessage: function (msg) {
-    var html = msg + "<br>";
-    this.consoleNode.html(this.consoleNode.html() + html);
-    this.consoleNode.scrollTop(this.consoleNode[0].scrollHeight);
-  },
-
-  ability: function (monsterName, abilityName) {
-    var msg = "<span class='monsterName'>"+monsterName+"</span> used <span class='abilityName'>"+abilityName+"</span> !";
-    Console.archieveMessage(msg);
-  },
-
-  damage: function (monsterName, hpLost, percentHpLost) {
-    var msg = "<span class='monsterName'>"+monsterName+"</span> lost <span class='number'>"+hpLost+"</span> ("+percentHpLost+"%) HP !";
-    Console.archieveMessage(msg);
-  },
-
-  heal: function (monsterName, hpHealed, percentHpHealed) {
-    var msg = "<span class='monsterName'>"+monsterName+"</span> restored <span class='number'>"+hpHealed+"</span> ("+percentHpHealed+"%) HP !";
-    Console.archieveMessage(msg);
-  },
-
-  effective: function () {
-    Console.archieveMessage("It's super effective !");
-  },
-
-  ineffective: function () {
-    Console.archieveMessage("It's not very effective...");
-  },
-
-  turn: function (turnNumber) {
-    var msg= "<hr><span class='turnNumber'>Turn " + turnNumber + "</span>";
-    Console.archieveMessage(msg);
-  },
-
-  dead: function (monsterName) {
-    var msg= "<span class='monsterName'>" + monsterNumber + "</span> is K.O.";
-    Console.archieveMessage(msg);
-  },
-
-  end: function () {
-    var msg= "The fight is over !";
-    Console.archieveMessage(msg);
-  },
-
-  victory: function (userName) {
-    var msg= "<span class='monsterName'>" + userName + "</span> wins !";
-    Console.archieveMessage(msg);
-  }
 }
--- app/static/js/dialog.js
+++ app/static/js/dialog.js
@@ -1,9 +1,66 @@
 Dialog = {
+  id: "#dialog",
+  
   show: function () {
-    $("#dialog").slideDown()
+    $(Dialog.id).slideDown()
   },
 
   hide: function () {
-    $("#dialog").slideUp()
+    $(Dialog.id).slideUp()
+  },
+  
+  // plain text
+  print: function (msg) {
+    $(Dialog.id).text(msg)
+  },
+  
+  // html
+  html: function (html) {
+    $(Dialog.id).html(html)
+  },
+  
+  Messages: {
+    ability: function (monsterName, abilityName) {
+      var html = "<span class='monsterName'>"+monsterName+"</span> used <span class='abilityName'>"+abilityName+"</span> !";
+      Dialog.html(html);
+    },
+    
+    damage: function (monsterName, hpLost, percentHpLost) {
+      var html = "<span class='monsterName'>"+monsterName+"</span> lost <span class='number'>"+hpLost+"</span> HP (<span class='number'>"+percentHpLost+"</span>%) !";
+      Dialog.html(html);
+    },
+    
+    heal: function (monsterName, hpHealed, percentHpHealed) {
+      var html = "<span class='monsterName'>"+monsterName+"</span> restored <span class='number'>"+hpHealed+"</span> HP (<span class='number'>"+percentHpHealed+"</span>%) !";
+      Dialog.html(html);
+    },
+    
+    effective: function () {
+      Dialog.print("It's super effective !");
+    },
+    
+    ineffective: function () {
+      Dialog.print("It's not very effective...");
+    },
+    
+    turn: function (turnNumber) {
+      var html = "<hr><span class='turnNumber'>Turn " + turnNumber + "</span>";
+      Dialog.html(html);
+    },
+    
+    dead: function (monsterName) {
+      var html = "<span class='monsterName'>" + monsterNumber + "</span> is K.O.";
+      Dialog.html(html);
+    },
+    
+    end: function () {
+      var msg = "The fight is over !";
+        Dialog.print(msg);
+    },
+    
+    victory: function (userName) {
+      var html = "<span class='monsterName'>" + userName + "</span> wins !";
+      Dialog.html(html);
+    }
   }
 }
